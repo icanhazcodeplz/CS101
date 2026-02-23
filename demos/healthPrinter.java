4d88d24466fadf1cd55cf8978146e2f503a261bf
icanhazcodeplz <67756774+icanhazcodeplz@users.noreply.github.com>
2026-02-23 12:26:42 -0500

    initial commit

---
+++ demos/healthPrinter.java
@@ -0,0 +1,12 @@
+/*
+ - If health is at least 80, print "High Health"
+ - If health is less than 80 but at least 20, print "Normal Health"
+ - If health is less than 20, print "Low Health"
+ - If health is at least 80 and you have an Energy Bar, print "Consider selling EnergyBar"
+ - If health is less than 20 and and you have an Energy Bar, print "Consider eating EnergyBar"
+ */
+
+public class healthPrinter {
+    public static void main(String[] args) {
+    }
+}
