e9d8964ca5e0d602385c688e5b8413f603eaf50a
icanhazcodeplz <67756774+icanhazcodeplz@users.noreply.github.com>
2026-03-02 09:36:32 -0500

    initial commit

---
+++ demos/ifForWhile.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class ifForWhile {
+    public static void main(String[] args) {
+        int x = 3;
+
+        if (x > 1) {
+            System.out.print("a ");
+        }
+
+        if (x > 2) {
+            System.out.print("b ");
+        }
+
+        if (x < 4) {
+            System.out.print("c ");
+        }
+
+        /*
+        - Add else to first
+        - Convert to else-if
+        - Add an else
+        - Basic for-loop
+        - including 5
+        - start at 1
+        - increase by 2
+        - decrease by 3
+        - change var name
+        - delare var outside loop
+        - show "start" just line above, "how" just line at end
+        - while loop count to 5
+        - while loop to guess users number
+         */
+//        for (int i = 0; i <= 5; i++) {
+//            System.out.println(i);
+//        }
+
+//        int i = 0;
+//        while (i <= 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        int userGuess = 0;
+//        while (userGuess != 8) {
+//            userGuess = scanner.nextInt();
+//            System.out.println(userGuess);
+//        }
+
+    }
+}
