67ae1322e7746fac164204508af04a5e17c3b7e5
icanhazcodeplz <67756774+icanhazcodeplz@users.noreply.github.com>
2026-03-03 11:12:43 -0500

    initial commit.

---
+++ labs/lab8_while_loops/lab8WhileLoops.java
@@ -0,0 +1,64 @@
+// 1. import the Scanner class
+public class lab8WhileLoops {
+    public static void main(String[] args) {
+
+        System.out.println("\n--- Print 1 through 5 (for loop) ---");
+        // 2. Write a for-loop that prints the numbers 1 through 5
+
+
+        System.out.println("\n--- Print 1 through 5 (while loop) ---");
+        // 3. Now do the same thing using a while loop (print the numbers 1 through 5)
+        // Hint: create an int variable called `i` and set it to 1 before the loop,
+        //       then increment it with `i++` inside the loop
+
+
+        System.out.println("\n--- Counting Down by Twos (for loop) ---");
+        // 4. Write a for-loop that prints the numbers from 10 to 2, counting down by 2 each time
+        // Hint: You might use `i -= 2` somewhere in your for loop definition
+
+
+        System.out.println("\n--- Counting Down by Twos (while loop) ---");
+        // 5. Now do the same thing using a while loop
+
+
+        System.out.println("\n--- Accumulating a Sum ---");
+        // 6. Create an int variable called `sum` and set it to 0
+        // 7. Write a while loop that adds the numbers 1 through 10 to `sum`
+
+        // TODO: Uncomment the line below
+        // System.out.println("Sum of 1 to 10: " + sum);
+
+
+        System.out.println("\n--- Input Validation ---");
+        // 8. Instantiate a Scanner object and call it `scanner`.
+
+        // TODO: Uncomment the lines below
+        // System.out.print("Enter a number between 1 and 10: ");
+        // int number = scanner.nextInt();
+
+        // 9. Write a while loop that keeps asking the user to enter a number
+        //    as long as the number is NOT between 1 and 10 (inclusive)
+        //    Inside the loop:
+        //      - print "Invalid! Try again: "
+        //      - read a new value into `number` using scanner.nextInt()
+
+        // TODO: Uncomment the line below
+        // System.out.println("You entered: " + number);
+
+
+        System.out.println("\n--- Do-While Loop ---");
+
+        // A do-while loop always runs at least once, then checks the condition.
+        // Syntax:
+        //   do {
+        //       // code
+        //   } while (condition);
+
+        // 10. Write a do-while loop that prints the numbers 1 through 5
+        //     - Create an int variable called `i` and set it to 1
+        //     - Inside the do block, print `i` and then increment it
+        //     - The loop should continue while `i` is less than or equal to 5
+
+
+    }
+}
